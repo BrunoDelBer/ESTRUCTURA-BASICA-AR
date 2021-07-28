b98ba5d72296e0ac924805bb74f8e97bec853d0b
BrunoDelBer <brunomar.369@gmail.com>
2021-07-28 18:48:10 -0500

    Create ESTRUCTURA_BASICA.ino
    PRIMMER COMMIT

---
+++ ESTRUCTURA_BASICA.ino
@@ -0,0 +1,29 @@
+
+/*
+ *Estructura BAsica
+ *Por: Bruno Omar 
+ *Fecha: 28 de junio del 2021
+ *
+ *Muestra basica de la estructura Basica de un programa
+ */
+
+
+//BIBLIOTECAS
+//CONSTANTES
+//VARIABLES
+//DEFINICIONES
+
+//SE EJECUTA AL INICIO AL ENERGIZAR Y UNA VEZ
+void setup() {//void setup inicio
+  // CODIGO
+
+}// void setup fin
+
+
+//CUERPO DEL PROGRAMA - CONSTANTE
+void loop() {//void loop inicio
+  // put your main code here, to run repeatedly:
+
+}//void loop final
+
+//FUNCIONES USUARIO
